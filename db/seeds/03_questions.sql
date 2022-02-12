feb56432ef74ecac26e230a68c68cd3720b6533e
Jimmy Chuk <jimmychuk@outlook.com>
2022-02-12 12:10:24 -0500

    add testing question and option seed data

---
+++ db/seeds/03_questions.sql
@@ -0,0 +1,2 @@
+INSERT INTO questions (id, quiz_id, value) 
+VALUES (1, 1, 'What is an asynchronous function?');
---
+++ db/seeds/04_options.sql
@@ -0,0 +1,5 @@
+INSERT INTO options (id, question_id, value , is_correct)
+VALUES (1, 1, 'toaster', FALSE),
+(2, 1, 'It''s kind of like a synchronous function.', FALSE),
+(3, 1, 'I ask myself that everyday...', TRUE),
+(4, 1, 'skip...', FALSE);
