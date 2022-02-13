4e25308df46b5d71a099c6d172df591bf3d35008
Lea Pehar <leapehar@gmail.com>
2022-02-13 15:24:36 +0000

    added hamburger menu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,11 @@
 // Client facing scripts here
+
+// hamburger menu
+
+
+const hamburger = document.getElementById('hamburger');
+let navUl = document.getElementsByClassName('navSection');
+
+hamburger.addEventListener('click', () => {
+  navUl.classList.toggle('show');
+})
