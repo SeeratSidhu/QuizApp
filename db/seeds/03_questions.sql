97ddfcb69d6eeb273da3d6d1fb258cc06a4f4cf3
Lea Pehar <leapehar@gmail.com>
2022-02-15 23:09:38 +0000

    added public quiz data to questions table

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -9,3 +9,17 @@ VALUES (3, 1, 'What is a bird?');
 
 INSERT INTO questions (id, quiz_id, value)
 VALUES (4, 2, 'What is an object?');
+
+
+
+-- adding public quiz questions
+
+
+INSERT INTO questions (id, quiz_id, value)
+VALUES (5, 100, 'question 1?');
+
+INSERT INTO questions (id, quiz_id, value)
+VALUES (6, 100, 'question 2?');
+
+INSERT INTO questions (id, quiz_id, value)
+VALUES (7, 100, 'question 3?');
