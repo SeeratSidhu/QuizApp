f34d6af71f1b7b854c74cf125ee42425c331313f
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-02-15 10:10:32 -0500

    Set up route to shared link

--- routes/results.js
+++ routes/results.js
@@ -39,7 +39,16 @@ module.exports = (db) => {
       console.log("Error: ", err.message);
     });
 
+  });
 
+  router.get("/:id", (req, res) => {
+    const result_id = req.params.id;
+    const queryString = `SELECT quizzes.id AS quiz_id, quizzes.name AS quiz_name, COUNT(questions.id) AS num_of_questions, quizzes.created_at, results.value, results.owner_id FROM questions JOIN quizzes ON quizzes.id = questions.quiz_id JOIN results ON results.quiz_id = quizzes.id WHERE results.id = $1 GROUP BY quizzes.id, results.value, results.owner_id;`
+    const values = [Number(result_id)];
+    db.query(queryString, values)
+    .then((data) => {
+      console.log(data.rows);
+    })
   });
   return router;
 };
