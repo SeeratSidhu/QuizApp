440fd1c70716f0abbacf244f9984d36fef2c3e49
Lea Pehar <leapehar@gmail.com>
2022-02-15 23:14:15 +0000

    no changes made here

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -4,7 +4,3 @@ VALUES (10001, 'Alice', 'Alice@greenwoods.com', 'password');
 
 INSERT INTO users (id, name, email, password)
 VALUES (10002, 'Kira', 'kira@hendon.com', 'password');
-
-
--- adding a public quiz
-
