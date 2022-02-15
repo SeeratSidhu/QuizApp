99a9b0baeec0945efe6ff45dc4a844cb18f62431
Lea Pehar <leapehar@gmail.com>
2022-02-15 23:15:46 +0000

    added public quiz data to users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -4,3 +4,6 @@ VALUES (10001, 'Alice', 'Alice@greenwoods.com', 'password');
 
 INSERT INTO users (id, name, email, password)
 VALUES (10002, 'Kira', 'kira@hendon.com', 'password');
+
+
+-- adding public table data
