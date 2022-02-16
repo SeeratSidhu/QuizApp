d7537ad0427764587225c25730cccfadb32259e7
Lea Pehar <leapehar@gmail.com>
2022-02-16 22:57:14 +0000

    debugged routes in order to render ejs templates and header partial

--- routes/library.js
+++ routes/library.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 
 module.exports = (db) => {
@@ -8,12 +8,12 @@ module.exports = (db) => {
   //move to quizzes later - quizzes:id
   router.get("/", (req, res) => {
     let sessionID = req.session.user_id;
-    if(!sessionID){
+    if (!sessionID) {
       return res.redirect("/");
     }
-    
+
     db.query(`
-    SELECT quizzes.id, quizzes.owner_id, is_active, quizzes.name, TO_CHAR(quizzes.created_at, 'YYYY/MM/DD') as created_at, 
+    SELECT quizzes.id, quizzes.owner_id, is_active, quizzes.name, TO_CHAR(quizzes.created_at, 'YYYY/MM/DD') as created_at,
     count(DISTINCT questions.id) as number_of_questions, count(results.id) as number_of_plays
     FROM quizzes
     JOIN questions ON questions.quiz_id = quizzes.id
@@ -22,17 +22,17 @@ module.exports = (db) => {
     GROUP BY quizzes.id
     ORDER BY created_at DESC, number_of_plays DESC;
     `, [sessionID])
-    .then((result)=>{
-      console.log(result.rows);
-      return res.send(result.rows);
-    })
-    .catch(err => {
-      console.log(err.msg);
-      return res.redirect("/");
-    })
+      .then((result) => {
+        console.log(result.rows);
+        return res.send(result.rows);
+      })
+      .catch(err => {
+        console.log(err.msg);
+        return res.redirect("/");
+      })
 
 
-    
+
   })
 
   return router;
