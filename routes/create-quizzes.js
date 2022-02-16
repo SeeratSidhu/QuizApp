f4f385afe28e58242bf0940e4a6ee9716e17afc7
Lea Pehar <leapehar@gmail.com>
2022-02-16 22:43:43 +0000

    debugged route to be able to render the header partial

--- routes/create-quizzes.js
+++ routes/create-quizzes.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const router  = express.Router();
-const { generateRandomInteger } = require("../helpers/create-random-integer");
+const router = express.Router();
+const {generateRandomInteger} = require("../helpers/create-random-integer");
 
 
 module.exports = (db) => {
 
   router.get("/", (req, res) => {
+
     let session = req.session;
-    if(!session.user_id){
+    if (!session.user_id) {
       return res.redirect("/login");
     }
-    res.render("new-quiz");
+    res.render("new-quiz", {user: session.user_id});
   });
 
   //adds quiz and returns quiz id
@@ -20,60 +21,60 @@ module.exports = (db) => {
 
     const id = generateRandomInteger();
     const values = [id, userID, req.body.title];
-  
+
     return db.query(`INSERT
     INTO quizzes (id, owner_id, name)
     VALUES ($1, $2, $3)
     RETURNING *;
-    `,values)
-      .then((result)=> {
-        console.log("new quiz ... ",result.rows[0]);
+    `, values)
+      .then((result) => {
+        console.log("new quiz ... ", result.rows[0]);
         res.send(result.rows[0]);
       })
       .catch(err => console.log(err.msg));
-    
+
   });
 
 
   //add question and returns question id
-  router.post("/:quizId/questions", (req,res) => {
+  router.post("/:quizId/questions", (req, res) => {
     const question_id = generateRandomInteger();
-    const values = [question_id , req.body.quiz_id, req.body.question_text];
-  
+    const values = [question_id, req.body.quiz_id, req.body.question_text];
+
     return db.query(`INSERT
     INTO questions(id, quiz_id, value)
     VALUES($1, $2, $3)
     RETURNING *;
     `, values)
-      .then((result)=> {
-        console.log("new question ... ",result.rows[0]);
+      .then((result) => {
+        console.log("new question ... ", result.rows[0]);
         res.send(result.rows[0]);
       })
       .catch(err => console.log(err.msg));
-  
+
   });
 
 
   //adds options to a question
-  router.post("/:quizId/:questionId/options", (req,res) => {
+  router.post("/:quizId/:questionId/options", (req, res) => {
     const option_id = generateRandomInteger();
     const values = [option_id, req.body.question_id, req.body.value, req.body.is_correct];
-  
+
     return db.query(`INSERT
     INTO options(id, question_id, value, is_correct)
     VALUES($1, $2, $3, $4)
     RETURNING *;
     `, values)
-      .then((result)=> {
-        console.log("new option ... ",result.rows[0]);
+      .then((result) => {
+        console.log("new option ... ", result.rows[0]);
         res.send(result.rows[0]);
-      // res.redirect("/");
+        // res.redirect("/");
       })
       .catch(err => console.log(err.msg));
-  
+
   });
 
 
-  
+
   return router;
 };
